9dc82ef5445e2e67c7d8ac06d0f9eaf265f53896
labo112 <laboexpe@outlook.fr>
2022-03-08 13:32:15 +0100

    MaJ table écriture - alpha gestion fonction2bin

--- serveur-modbus.js
+++ serveur-modbus.js
@@ -1,7 +1,27 @@
 import * as ModbusRTU from 'modbus-serial' ;
 
-let bufferUInt32 = Buffer.alloc(4)
-let binaire4 = ''
+let bufferFonction = Buffer.alloc(2) //mot 1
+let bufferDspc = Buffer.alloc(4) //mot double 2/3
+let bufferConsigne = Buffer.alloc(4) //mot double 4/5
+let bufferMasseVolumique = Buffer.alloc(2) //mot 6
+let bufferQuaiInjecteur = Buffer.alloc(2) //mot 7
+let bufferVolumeAdditif = Buffer.alloc(2) //mot 8
+let bufferTauxAdditif = Buffer.alloc(2) //mot 9
+
+let fonction = {
+    bras_service: 0,
+    dde_debit:0,
+    dde_arret:0,
+    dde_solde:0,
+    dde_synchro_heure:0,
+    acquit_alarme:0,
+    autorisation: 0,
+    cr_ok:0,
+    bras_sud:0,
+    bars_nord:0
+}
+
+
 
 var vector = {
     getInputRegister: function(addr, unitID) {
@@ -25,8 +45,8 @@ var vector = {
     },
     setRegister: function(addr, value, unitID) {
         // Asynchronous handling supported also here
-      setConsigne(addr,value)
-        .then((buffer) =>console.log("Buffer 32 - Consigne saisie : ", buffer.readUInt32BE(0).toString(10)))
+      tableEcriture(addr,value)
+        .then((buffer) =>console.log("Valeur saisie : ", buffer))
         .catch(err => console.log("Promise erreur : ",err))
         return;
     },
@@ -116,16 +136,16 @@ function setRegisterSwitch(addr, value) {
             console.log("N° DSPC", dspc);
             break;
         case 4:
-            console.log("Buffer :", bufferUInt32);
-            bufferUInt32.fill(value)
-            console.log("Buffer :", bufferUInt32);
+            console.log("Buffer :", bufferConsigne);
+            bufferConsigne.fill(value)
+            console.log("Buffer :", bufferConsigne);
             break;
         case 5:
             prede.push(value);
-            bufferUInt32.fill(prede)
+            bufferConsigne.fill(prede)
             console.log("Prédétermination");
             console.log("Longueur Tableau : ", prede.length)
-            console.log("Buffer :", bufferUInt32);
+            console.log("Buffer :", bufferConsigne);
 
             if(prede.length >0) {
                 
@@ -169,23 +189,78 @@ function setRegisterSwitch(addr, value) {
     }
 }
 
-async function setConsigne(addr,value) {
+async function tableEcriture(addr,value) {
     return await new Promise ((resolve, reject) => {
 
-        console.log("setConsigne")
-        console.log("Valeur Buffer initial :", bufferUInt32);
+        console.log("tableEcriture")
+
+        if(addr === 0) {
+            bufferFonction.writeUInt16BE(value,0)
+            console.log("Buffer  :", bufferFonction);
+            console.log("Binaire : ", Array.from(bufferFonction.readUInt16BE(0).toString(2)))
+            fonction2Bin(Array.from(bufferFonction.readUInt16BE(0).toString(2)), fonction)
+            resolve(bufferFonction.readUInt16BE(0).toString(10))
+        }
+
+        if(addr === 2) {
+            bufferDspc.writeUInt16BE(value,2)
+            console.log("Buffer  :", bufferConsigne);
+            //reject()
+        }
+        if (addr === 3) {
+            bufferDspc.writeUInt16BE(value,0)
+            console.log("Buffer : ", bufferDspc )
+            resolve(bufferDspc.readUInt32BE(0).toString(10))
+        }
 
         if(addr === 4) {
-            bufferUInt32.writeUInt16BE(value,2)
-            console.log("Buffer 4 :", bufferUInt32);
+            bufferConsigne.writeUInt16BE(value,2)
+            console.log("Buffer 4 :", bufferConsigne);
             //reject()
         }
         if (addr === 5) {
-            bufferUInt32.writeUInt16BE(value,0)
-            console.log("Buffer addr 5 : ", bufferUInt32 )
-            resolve(bufferUInt32)
+            bufferConsigne.writeUInt16BE(value,0)
+            console.log("Buffer addr 5 : ", bufferConsigne )
+            resolve(bufferConsigne.readUInt32BE(0).toString(10))
+        }
+
+        if(addr === 6) {
+            bufferMasseVolumique.writeUInt16BE(value,0)
+            console.log("Buffer  :", bufferMasseVolumique);
+            resolve(bufferMasseVolumique.readUInt16BE(0),toString(10))
+        }
+
+        if(addr === 7) {
+            bufferQuaiInjecteur.writeUInt16BE(value,0)
+            console.log("Buffer  :", bufferQuaiInjecteur);
+            resolve(bufferQuaiInjecteur.readUInt16BE(0).toString(10))
+        }
+
+        if(addr === 8) {
+            bufferVolumeAdditif.writeUInt16BE(value,0)
+            console.log("Buffer  :", bufferVolumeAdditif);
+            resolve(bufferVolumeAdditif.readUInt16BE(0).toString(10))
+        }
+
+        if(addr === 9) {
+            bufferTauxAdditif.writeUInt16BE(value,0)
+            console.log("Buffer  :", bufferTauxAdditif);
+            resolve(bufferTauxAdditif.readUInt16BE(0).toString(10))
         }
         
         //if (addr !== 4 || addr !== 5) err()
     })
+}
+
+async function fonction2Bin(array, objet) {
+ 
+    for (let index of array) {
+        console.log("Index : ", index)
+        for (let [key, value] of Object.entries(objet)){
+            key[value] = index;
+            console.log("Valeur : ", key,value)
+        }
+    }
+    console.log("Objet : ", objet)
+    return objet
 }
